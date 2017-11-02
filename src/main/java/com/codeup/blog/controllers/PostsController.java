14f50dd2c3cdbcf32697d134acc6c689c5a15708
amyyan210 <amyyan210@gmail.com>
2017-11-02 13:49:55 -0500

    Add parameter to view single post method

--- src/main/java/com/codeup/blog/controllers/PostsController.java
+++ src/main/java/com/codeup/blog/controllers/PostsController.java
@@ -2,6 +2,7 @@ package com.codeup.blog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,19 +17,19 @@ public class PostsController {
 
     @GetMapping("/posts/{id}")
     @ResponseBody
-    public String viewPost () {
+    public String viewPost (@PathVariable int id) {
         return "view an individual post";
     }
 
     @GetMapping("/posts/create")
     @ResponseBody
-    public String formPosts () {
+    public String viewCreateForm () {
         return "view the form for creating a post";
     }
 
     @PostMapping("/posts/create")
     @ResponseBody
-    public String createPosts () {
+    public String createPost () {
         return "create a new post";
     }
 
