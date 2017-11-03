486762f16d6ad9c01c362bf9211e03f6d5f22db4
amyyan210 <amyyan210@gmail.com>
2017-11-03 10:29:23 -0500

    * Create views for showing all posts and single post * Modify existing controllers to pass to these views

---
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -0,0 +1,24 @@
+package com.codeup.blog.models;
+
+public class Post {
+
+    private String title;
+    private String body;
+
+    public String getTitle () {
+        return this.title;
+    }
+
+    public String getBody () {
+        return this.body;
+    }
+
+    public void setTitle (String title) {
+        this.title = title;
+    }
+
+    public void setBody (String body) {
+        this.body = body;
+    }
+
+}
